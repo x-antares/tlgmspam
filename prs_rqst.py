f7a16fa86372526a3d5dd6bc49a8e65a7d667ea6
Oleksandr <redko.oleksandr@webmaestro.com.ua>
2022-04-17 22:10:13 +0300

    remove trash

--- prs_rqst.py
+++ prs_rqst.py
@@ -1,8 +1,5 @@
 from telethon.sync import TelegramClient
 from telethon import functions, types
-#from conf import api_id
-#from conf import api_hash
-#from conf import phone
 
 api_id = 14705741
 api_hash = '28680abc0e9e0a5f8d0b85cc8a45c8bb'
