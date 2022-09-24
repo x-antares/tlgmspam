91d882734e782ea888a718b42e4b310205c06c62
San of Alexandria <47152889+0Jac0k19D01rupal0@users.noreply.github.com>
2022-09-24 14:31:27 +0300

    fix encode file input

--- str_get_ids.py
+++ str_get_ids.py
@@ -1,7 +1,7 @@
 import re
 pattern = re.compile("user_id=([0-9]{8,10})")
 result = ""
-with open('user_ids.txt', 'w') as file:
+with open('user_ids.txt', 'w', encoding="utf-8") as file:
     for i, line in enumerate(open('read.txt')):
         for match in re.finditer(pattern, line):
             result = match.group(1)
